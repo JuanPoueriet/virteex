0e44d88a970ba247889ea31651d8be357955f880
JuanPoueriet <juanpoueriet00@gmail.com>
2025-12-09 17:24:27 -0400

    feat(flags): add SVG files for Switzerland, Colombia, and the United States fix(auth): improve authentication guard logic

--- apps/core/client-web/src/app/core/guards/auth-guard.ts
+++ apps/core/client-web/src/app/core/guards/auth-guard.ts
@@ -5,12 +5,20 @@ import { CanActivateFn, Router, UrlTree } from '@angular/router';
 import { AuthService } from '../services/auth';
 import { LanguageService } from '../services/language';
 import { map, take } from 'rxjs/operators';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+
+
 
 export const authGuard: CanActivateFn = (): Observable<boolean | UrlTree> => {
   const authService = inject(AuthService);
   const router = inject(Router);
   const languageService = inject(LanguageService);
+
+  
+  if (authService.isAuthenticated()) {
+    return of(true);
+  }
 
   return authService.checkAuthStatus().pipe(
     take(1),
